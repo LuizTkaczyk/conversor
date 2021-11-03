500b7b504c76db6eeb38e149721c4ab79cbb7d50
Luiz Antonio <luiz8907@hotmail.com>
2021-11-03 16:24:32 -0300

    conversor angular

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components'; 
 import { MoedaService, ConversorService } from './services';
 import { HttpClientModule } from '@angular/common/http';
+import { NumeroDirective } from './directives/numero.directive';
 
 
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    NumeroDirective
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   exports:[
     ConversorComponent
